bb9b2e42b8cb9b1749f203aae0077a69b07d4052
Michael <ms.esipov@gmail.com>
2023-03-21 22:19:04 +0500

    First commit. Created new project

---
+++ src/main/java/socialnet/SocialNetApp.java
@@ -0,0 +1,11 @@
+package socialnet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialNetApp {
+    public static void main(String[] args) {
+        SpringApplication.run(SocialNetApp.class, args);
+    }
+}
