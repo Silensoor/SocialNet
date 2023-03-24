b4b7931d94e30a692dd5ce521540e6934b0632d5
Michael <ms.esipov@gmail.com>
2023-03-24 21:30:04 +0500

    Created models. Removed tomcat from pom.xml

---
+++ src/main/java/socialnet/model/PostComment.java
@@ -0,0 +1,17 @@
+package socialnet.model;
+
+import lombok.Data;
+
+import java.sql.Timestamp;
+
+@Data
+public class PostComment {
+    private long id;
+    private String commentText;
+    private boolean isBlocked;
+    private boolean isDeleted;
+    private Timestamp time;
+    private long parentId;
+    private long authorId;
+    private long postId;
+}
