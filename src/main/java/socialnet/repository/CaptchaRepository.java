47ebe3de8c8ed1f6dc6d35062483d5b79bad5b4c
Snake <serebryannikov@lorena-kuhni.ru>
2023-03-28 08:20:38 +0500

    Register and Captcha

---
+++ src/main/java/socialnet/repository/CaptchaRepository.java
@@ -0,0 +1,45 @@
+package socialnet.repository;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+import socialnet.model.Captcha;
+
+@Repository
+@RequiredArgsConstructor
+public class CaptchaRepository {
+    private final JdbcTemplate jdbcTemplate;
+
+    public int saveCaptcha(Captcha captcha) {
+        return jdbcTemplate.update(
+            "insert into captcha (code, secret_code, time) values (?, ?, ?)",
+            captcha.getCode(),
+            captcha.getSecretCode(),
+            captcha.getTime()
+        );
+    }
+
+    public Captcha findCaptchaBySecretCode(String secretCode) {
+        try {
+            return jdbcTemplate.queryForObject(
+                "select * from captcha where secret_code = ?",
+                captchaRowMapper,
+                secretCode
+            );
+        } catch (EmptyResultDataAccessException ignored) {
+            return null;
+        }
+    }
+
+    private final RowMapper<Captcha> captchaRowMapper = (resultSet, rowNum) -> {
+        Captcha captcha = new Captcha();
+        captcha.setId(resultSet.getLong("id"));
+        captcha.setCode(resultSet.getString("code"));
+        captcha.setSecretCode(resultSet.getString("secret_code"));
+        captcha.setTime(resultSet.getTimestamp("time"));
+
+        return captcha;
+    };
+}
