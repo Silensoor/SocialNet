1e4d9172328292d8aa2cf43e9c0586ff2add9c65
Snake <serebryannikov@lorena-kuhni.ru>
2023-04-13 14:59:04 +0500

    Posts testing

--- src/test/java/socialnet/PostsTest.java
+++ src/test/java/socialnet/PostsTest.java
@@ -1,12 +1,11 @@
 package socialnet;
 
 import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.HttpStatus;
-import org.springframework.security.test.context.support.WithAnonymousUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import socialnet.controller.PostsController;
@@ -16,6 +15,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.security.test.web.servlet.response.SecurityMockMvcResultMatchers.unauthenticated;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
@@ -36,13 +36,14 @@ public class PostsTest {
     }
 
     @Test
+    @DisplayName("Загрузка контекста")
     public void contextLoads() {
         assertThat(postsController).isNotNull();
         assertThat(jwtUtils).isNotNull();
     }
 
     @Test
-    @WithAnonymousUser
+    @DisplayName("Неавторизованный пользователь")
     public void accessDenied() throws Exception {
         mockMvc
             .perform(get("/api/v1/post/1"))
@@ -52,20 +53,23 @@ public class PostsTest {
     }
 
     @Test
+    @DisplayName("Получение поста по существующему ID")
     public void getPostByExistsId() throws Exception {
         mockMvc
             .perform(get("/api/v1/post/1").header("Authorization", getToken()))
             .andDo(print())
             .andExpect(status().isOk())
+            .andExpect(content().contentType("application/json"))
             .andReturn();
     }
 
     @Test
+    @DisplayName("Получение поста по несуществующему ID")
     public void getPostByNotExistsId() throws Exception {
         mockMvc
             .perform(get("/api/v1/post/0").header("Authorization", getToken()))
             .andDo(print())
-            .andExpect(status().is(HttpStatus.NOT_FOUND.value()))
+            .andExpect(status().isNotFound())
             .andReturn();
     }
 }
