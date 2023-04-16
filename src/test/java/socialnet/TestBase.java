0ead661e15f2a91e20ab2074522f05d4c53a416c
Snake174 <jonnysereb@gmail.com>
2023-04-17 00:28:49 +0500

    TESTS AAAAAAAAAAAAAAAAAAAA!!!!

---
+++ src/test/java/socialnet/TestBase.java
@@ -0,0 +1,41 @@
+package socialnet;
+
+import org.jetbrains.annotations.NotNull;
+import org.junit.ClassRule;
+import org.junit.jupiter.api.BeforeAll;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.util.TestPropertyValues;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.jdbc.Sql;
+import org.testcontainers.containers.PostgreSQLContainer;
+
+@Sql("/sql/schema.sql")
+@Sql("/sql/data.sql")
+@ActiveProfiles("test")
+@SpringBootTest
+@AutoConfigureMockMvc
+@ContextConfiguration(initializers = TestBase.Initializer.class)
+public abstract class TestBase {
+    @ClassRule
+    public static final PostgreSQLContainer<?> container = new PostgreSQLContainer<>("postgres:12.14");
+
+    public static class Initializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+        @Override
+        public void initialize(@NotNull ConfigurableApplicationContext configurableApplicationContext) {
+            TestPropertyValues.of(
+                "spring.datasource.url=" + container.getJdbcUrl(),
+                "spring.datasource.username=" + container.getUsername(),
+                "spring.datasource.password=" + container.getPassword()
+            ).applyTo(configurableApplicationContext);
+        }
+    }
+
+    @BeforeAll
+    public static void init() {
+        container.start();
+    }
+}
