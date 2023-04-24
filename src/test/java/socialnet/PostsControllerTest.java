354d59806bbdf77be6bd7fbb0e716e52584dd230
Snake174 <jonnysereb@gmail.com>
2023-04-25 00:17:34 +0500

    Tests

--- src/test/java/socialnet/PostsControllerTest.java
+++ src/test/java/socialnet/PostsControllerTest.java
@@ -1,10 +1,23 @@
 package socialnet;
 
+import org.jetbrains.annotations.NotNull;
+import org.junit.ClassRule;
 import org.junit.Test;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import socialnet.controller.PostsController;
-import socialnet.exception.EntityNotFoundException;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.util.TestPropertyValues;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+import org.testcontainers.containers.PostgreSQLContainer;
+import socialnet.security.jwt.JwtUtils;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.hamcrest.CoreMatchers.containsString;
@@ -16,44 +29,72 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-public class PostsControllerTest extends AbstractTest {
+@RunWith(SpringRunner.class)
+@ActiveProfiles("test")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+@ContextConfiguration(initializers = { PostsControllerTest.Initializer.class })
+public class PostsControllerTest {
     @Autowired
-    private PostsController postsController;
+    private MockMvc mockMvc;
+
+    @Autowired
+    private JwtUtils jwtUtils;
 
     private final String TEST_EMAIL = "user1@email.com";
 
+    @ClassRule
+    public static final PostgreSQLContainer<?> container = new PostgreSQLContainer<>("postgres:12.14");
+
+    public static class Initializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+        @Override
+        public void initialize(@NotNull ConfigurableApplicationContext configurableApplicationContext) {
+            TestPropertyValues.of(
+                "spring.datasource.url=" + container.getJdbcUrl(),
+                "spring.datasource.username=" + container.getUsername(),
+                "spring.datasource.password=" + container.getPassword()
+            ).applyTo(configurableApplicationContext.getEnvironment());
+        }
+    }
+
+    private String getToken(String email) {
+        return jwtUtils.generateJwtToken(email);
+    }
+
     @Test
     @DisplayName("Загрузка контекста")
+    @Transactional
     public void contextLoads() {
-        /*assertThat(mockMvc).isNotNull();
-        assertThat(postsController).isNotNull();
+        assertThat(mockMvc).isNotNull();
         assertThat(jwtUtils).isNotNull();
-        assertThat(personService).isNotNull();*/
     }
 
     @Test
     @DisplayName("Неавторизованный пользователь")
+    @Transactional
     public void accessDenied() throws Exception {
-        /*mockMvc
+        mockMvc
             .perform(get("/api/v1/post/1"))
             .andDo(print())
             .andExpect(unauthenticated())
-            .andReturn();*/
+            .andReturn();
     }
 
     @Test
     @DisplayName("Получение поста по существующему ID")
+    @Transactional
     public void getPostByExistsId() throws Exception {
-        /*mockMvc
+        mockMvc
             .perform(get("/api/v1/post/1").header("authorization", getToken(TEST_EMAIL)))
             .andDo(print())
             .andExpect(status().isOk())
             .andExpect(content().contentType("application/json"))
-            .andReturn();*/
+            .andReturn();
     }
 
     @Test
     @DisplayName("Получение поста по несуществующему ID")
+    @Transactional
     public void getPostByNotExistsId() throws Exception {
         /*mockMvc
             .perform(get("/api/v1/post/0").header("authorization", getToken(TEST_EMAIL)))
