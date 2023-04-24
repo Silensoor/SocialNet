6da4e0381fedb115f009040046aca6c9203b67b4
alexsandr_isakov <agisakov@mail.ru>
2023-04-24 17:42:22 +0700

    search refactoring 24.04.2023

--- src/main/java/socialnet/service/TagService.java
+++ src/main/java/socialnet/service/TagService.java
@@ -23,12 +23,30 @@ public class TagService {
         }
     }
 
-    public List<Post2Tag> getPostByQueryTags(String[] tags){
+    public String getPostByQueryTags(String[] tags){
         List<Tag> tagList = tagRepository.getTagsByQuery(tags);
-        if (!tagList.isEmpty()) {
-            return post2TagRepository.getQuery(tagList);
+        List<Post2Tag> query;
+        if (tagList != null) {
+            query = post2TagRepository.getQuery(tagList);
+        } else {
+            return null;
+        }
+        StringBuilder sqlTags = new StringBuilder(" ");
+        if (query != null && !query.isEmpty()) {
+            for (Post2Tag post2Tag : query) {
+                if (post2Tag.getPostId() != 0) {
+                    sqlTags.append(" ").append(post2Tag.getId()).append(", ");
+                }
+            }
+            if (sqlTags.length() > 4) {
+                if (sqlTags.substring(sqlTags.length() - 2).equals(", ")) {
+                    sqlTags.delete(sqlTags.length() - 2, sqlTags.length());
+                }
+            }
+            return sqlTags.toString();
         } else {
             return null;
         }
     }
+
 }
