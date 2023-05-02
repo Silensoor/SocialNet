3cb18c271f40456829b6a64140ef4fee8e8edba2
Snake <serebryannikov@lorena-kuhni.ru>
2023-05-02 14:43:05 +0500

    Posts testing

--- src/main/java/socialnet/service/TelegramBotService.java
+++ src/main/java/socialnet/service/TelegramBotService.java
@@ -2,14 +2,15 @@ package socialnet.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestBody;
 import socialnet.api.request.TgApiRequest;
 import socialnet.repository.TelegramBotRepository;
+import socialnet.utils.MailSender;
 
 @Service
 @RequiredArgsConstructor
 public class TelegramBotService {
     private final TelegramBotRepository telegramBotRepository;
+    private final MailSender mailSender;
 
     public void register(long telegramId, String email, String cmd) {
         telegramBotRepository.register(telegramId, email, cmd);
@@ -18,5 +19,11 @@ public class TelegramBotService {
     public void execCommand(TgApiRequest request) {
         System.out.println("Telegram ID: " + request.getId());
         System.out.println("Command: " + request.getCommand());
+
+        try {
+            mailSender.send("jonnysereb@gmail.com", "Subject", "http://localhost:8086/api/v1/tg?id=" + request.getId() + "&cmd=register");
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
     }
 }
--- src/main/java/socialnet/utils/MailSender.java
+++ src/main/java/socialnet/utils/MailSender.java
@@ -1,6 +1,8 @@
 package socialnet.utils;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
 import javax.mail.*;
 import javax.mail.internet.InternetAddress;
@@ -9,27 +11,40 @@ import javax.mail.internet.MimeMessage;
 import javax.mail.internet.MimeMultipart;
 import java.util.Properties;
 
+@Component
+@RequiredArgsConstructor
 public class MailSender {
-    public MailSender() {
-    }
+    @Value("${mail.smtpHost}")
+    private String mailSmtpHost;
+
+    @Value("${mail.smtpPort}")
+    private String mailSmtpPort;
+
+    @Value("${mail.authUser}")
+    private String mailAuthUser;
+
+    @Value("${mail.authPass}")
+    private String mailAuthPass;
 
     public void send(String to, String subject, String text) throws Exception {
         Properties prop = new Properties();
         prop.put("mail.smtp.auth", true);
         prop.put("mail.smtp.starttls.enable", "true");
-        prop.put("mail.smtp.host", "smtp.mailtrap.io");
-        prop.put("mail.smtp.port", "25");
-        prop.put("mail.smtp.ssl.trust", "smtp.mailtrap.io");
+        prop.put("mail.smtp.host", mailSmtpHost);
+        prop.put("mail.smtp.port", mailSmtpPort);
+        prop.put("mail.smtp.ssl.trust", "*");
+        prop.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+        prop.put("mail.smtp.ssl.protocols", "TLSv1.2");
 
         Session session = Session.getInstance(prop, new Authenticator() {
             @Override
             protected PasswordAuthentication getPasswordAuthentication() {
-                return new PasswordAuthentication("username", "password");
+                return new PasswordAuthentication(mailAuthUser, mailAuthPass);
             }
         });
 
         Message message = new MimeMessage(session);
-        message.setFrom(new InternetAddress("from@gmail.com"));
+        message.setFrom(new InternetAddress(mailAuthUser, "Zerone SocialNet"));
         message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(to));
         message.setSubject(subject);
 
