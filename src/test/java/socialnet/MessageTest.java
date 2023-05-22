ca60671b30ec7c668ea4313218ff039ed5a1d420
Anton Anikanov <r4nv4ld@gmail.com>
2023-05-22 12:12:30 +0300

    Пишем тесты...

---
+++ src/test/java/socialnet/MessageTest.java
@@ -0,0 +1,57 @@
+package socialnet;
+
+import org.jetbrains.annotations.NotNull;
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.util.TestPropertyValues;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.request.RequestPostProcessor;
+import org.testcontainers.containers.PostgreSQLContainer;
+import socialnet.security.jwt.JwtUtils;
+
+@RunWith(SpringRunner.class)
+@ActiveProfiles("test")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+@ContextConfiguration(initializers = { MessageTest.Initializer.class })
+public class MessageTest {
+
+    @Autowired
+    private JwtUtils jwtUtils;
+
+    private final String TEST_EMAIL = "user1@email.com";
+
+    @ClassRule
+    public static final PostgreSQLContainer<?> container = new PostgreSQLContainer<>("postgres:12.14");
+
+    public static class Initializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+        @Override
+        public void initialize(@NotNull ConfigurableApplicationContext configurableApplicationContext) {
+            TestPropertyValues.of(
+                    "spring.datasource.url=" + container.getJdbcUrl(),
+                    "spring.datasource.username=" + container.getUsername(),
+                    "spring.datasource.password=" + container.getPassword()
+            ).applyTo(configurableApplicationContext.getEnvironment());
+        }
+    }
+
+    public RequestPostProcessor authorization() {
+        return request -> {
+            request.addHeader("authorization", jwtUtils.generateJwtToken(TEST_EMAIL));
+            return request;
+        };
+    }
+
+    @Test
+    public void editMessage() throws Exception {
+
+    }
+}
