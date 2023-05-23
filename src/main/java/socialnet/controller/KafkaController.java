8de7004dc5237094ea4b4049cb3f7cd66bbf1c98
Snake <serebryannikov@lorena-kuhni.ru>
2023-05-23 13:05:01 +0500

    Kafka

--- src/main/java/socialnet/controller/KafkaController.java
+++ src/main/java/socialnet/controller/KafkaController.java
@@ -2,20 +2,14 @@ package socialnet.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.kafka.support.SendResult;
-import org.springframework.messaging.handler.annotation.Payload;
-import org.springframework.util.concurrent.ListenableFuture;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.kafka.annotation.KafkaListener;
 import socialnet.api.request.KafkaMessageRq;
-
-import java.util.concurrent.CompletableFuture;
 
 @RequestMapping("/api/v1/kafka")
 @RestController
@@ -24,21 +18,19 @@ import java.util.concurrent.CompletableFuture;
 public class KafkaController {
     private final KafkaTemplate<String, KafkaMessageRq> kafkaTemplate;
 
+    @Value(value = "${spring.kafka.consumer.topic-name}")
+    private String topicName;
+
     @PostMapping
     public void sendMessage(@RequestBody KafkaMessageRq message) {
-        kafkaTemplate.send("transaction-1", message);
+        kafkaTemplate.send(topicName, message);
     }
 
-    /*@KafkaListener(topics = "transaction-1", groupId = "group-1")
-    public void listener(@Payload KafkaMessageRq message, ConsumerRecord<String, KafkaMessageRq> cr) {
-        log.info("Topic [transaction-1] Received message: {}", message.getData());
-        log.info(cr.toString());
-    }*/
-
     @KafkaListener(
-        topics = "transaction-1",
+        topics = "${spring.kafka.consumer.topic-name}",
+        groupId = "${spring.kafka.consumer.group-id}",
         containerFactory = "kafkaListenerContainerFactory")
     public void messageListener(KafkaMessageRq message) {
-        log.info("Topic [transaction-1] Received message: {}", message.getData());
+        log.info("Topic [${spring.kafka.consumer.topic-name}] Received message: {}", message.getData());
     }
 }
