34ef0809f8f3a8a72caf68f74e72c6d10bf7a2cf
Anton Anikanov <r4nv4ld@gmail.com>
2023-05-29 09:39:11 +0300

    Пишем тесты

--- src/test/java/socialnet/MessageTest.java
+++ src/test/java/socialnet/MessageTest.java
@@ -7,22 +7,52 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.util.TestPropertyValues;
 import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.RequestPostProcessor;
+import org.springframework.transaction.annotation.Transactional;
 import org.testcontainers.containers.PostgreSQLContainer;
+import socialnet.config.KafkaConsumerConfig;
+import socialnet.config.KafkaProducerConfig;
+import socialnet.config.KafkaTopicConfig;
+import socialnet.repository.MessageRepository;
+import socialnet.schedules.RemoveDeletedPosts;
+import socialnet.schedules.RemoveOldCaptchasSchedule;
+import socialnet.schedules.UpdateOnlineStatusScheduler;
 import socialnet.security.jwt.JwtUtils;
+import socialnet.service.KafkaService;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @ActiveProfiles("test")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
 @ContextConfiguration(initializers = { MessageTest.Initializer.class })
+@Sql({ "/sql/posts_controller_test.sql" })
+@MockBean(RemoveOldCaptchasSchedule.class)
+@MockBean(RemoveDeletedPosts.class)
+@MockBean(UpdateOnlineStatusScheduler.class)
+@MockBean(KafkaConsumerConfig.class)
+@MockBean(KafkaProducerConfig.class)
+@MockBean(KafkaTopicConfig.class)
+@MockBean(KafkaService.class)
 public class MessageTest {
+
+    @Autowired
+    private MessageRepository messageRepository;
+
+    @Autowired
+    private MockMvc mockMvc;
 
     @Autowired
     private JwtUtils jwtUtils;
@@ -51,7 +81,17 @@ public class MessageTest {
     }
 
     @Test
+    @Transactional
     public void editMessage() throws Exception {
+        Integer messageListStart = messageRepository.getAllMessage();
+        System.out.println("до: " + messageListStart);
 
+        this.mockMvc
+                .perform(get("/api/v1/dialogs").with(authorization()))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andReturn();
+
+        Integer messageListEnd = messageRepository.getAllMessage();
     }
 }
