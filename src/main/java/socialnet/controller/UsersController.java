557b741e26b7512390fa7af3abc0a4b863b6012c
alexsandr_isakov <agisakov@mail.ru>
2023-05-31 19:59:05 +0700

    OpenApi 31.05.2023 5

--- src/main/java/socialnet/controller/UsersController.java
+++ src/main/java/socialnet/controller/UsersController.java
@@ -48,37 +48,57 @@ public class UsersController {
 
     @OnlineStatusUpdatable
     @GetMapping("/{id}")
-    @Operation(summary = "get user by id")
+    @Operation(summary = "get user by id", responses = {
+            @ApiResponse(responseCode = "200", description = "OK",
+                    content = {@Content(schema = @Schema(ref = "#/components/schemas/CommonRsPersonRs"))}),
+            @ApiResponse(responseCode = "400", description = "Name of error",
+                    content = {@Content(schema = @Schema(implementation = ErrorRs.class))}),
+            @ApiResponse(responseCode = "401", description = "Unauthorized", content = {@Content(schema = @Schema())}),
+            @ApiResponse(responseCode = "403", description = "Forbidden", content = {@Content(schema = @Schema())})})
     public CommonRs<PersonRs> getUserById(@RequestHeader(name = "authorization")
                                               @Parameter(description =  "Access Token", example = "JWT Token")
                                               String authorization,
-                                          @PathVariable(name = "id") Integer id) {
+                                          @PathVariable(name = "id")
+                                          @Parameter(description =  "id", example = "1")Integer id) {
         return personService.getUserById(authorization, id);
     }
 
     @OnlineStatusUpdatable
     @GetMapping("/search")
-    @Operation(summary = "search post by query")
+    @Operation(summary = "search post by query", responses = {
+            @ApiResponse(responseCode = "200", description = "OK",
+                    content = {@Content(schema = @Schema(ref = "#/components/schemas/CommonRsListPersonRs"))}),
+            @ApiResponse(responseCode = "400", description = "Name of error",
+                    content = {@Content(schema = @Schema(implementation = ErrorRs.class))}),
+            @ApiResponse(responseCode = "401", description = "Unauthorized", content = {@Content(schema = @Schema())}),
+            @ApiResponse(responseCode = "403", description = "Forbidden", content = {@Content(schema = @Schema())})})
     public CommonRs<List<PersonRs>> findPersons(@RequestHeader
-                                                    @Parameter(description =  "Access Token", example = "JWT Token")
-                                                    String authorization,
+                                                @Parameter(description =  "Access Token", example = "JWT Token")
+                                                String authorization,
                                                 @RequestParam(name = "age_from", required = false, defaultValue = "0")
+                                                @Parameter(description =  "ageFrom", example = "5")
                                                 Integer ageFrom,
                                                 @RequestParam(name = "age_to", required = false, defaultValue = "0")
+                                                @Parameter(description =  "ageTo", example = "50")
                                                 Integer ageTo,
                                                 @RequestParam(required = false, defaultValue = "")
+                                                @Parameter(description =  "city", example = "city")
                                                 String city,
                                                 @RequestParam(required = false, defaultValue = "")
+                                                @Parameter(description =  "country", example = "country")
                                                 String country,
                                                 @RequestParam(name = "first_name", required = false, defaultValue = "")
+                                                @Parameter(description =  "firstName", example = "firstName")
                                                 String firstName,
                                                 @RequestParam(name = "last_name", required = false, defaultValue = "")
+                                                @Parameter(description =  "lastName", example = "lastName")
                                                 String lastName,
                                                 @RequestParam(required = false, defaultValue = "0")
+                                                @Parameter(description =  "offset", example = "0")
                                                 Integer offset,
                                                 @RequestParam(required = false, defaultValue = "20")
+                                                @Parameter(description =  "perPage", example = "20")
                                                 Integer perPage) {
-
 
         return findService.findPersons(SearchOptions.builder()
                 .jwtToken(authorization)
@@ -95,17 +115,30 @@ public class UsersController {
 
     @OnlineStatusUpdatable
     @PutMapping("/me")
-    @Operation(summary = "update information about me")
+    @Operation(summary = "update information about me", responses = {
+            @ApiResponse(responseCode = "200", description = "OK",
+                    content = {@Content(schema = @Schema(ref = "#/components/schemas/ResponseEntityCommonRsPersonRs"))}),
+            @ApiResponse(responseCode = "400", description = "Name of error",
+                    content = {@Content(schema = @Schema(implementation = ErrorRs.class))}),
+            @ApiResponse(responseCode = "401", description = "Unauthorized", content = {@Content(schema = @Schema())}),
+            @ApiResponse(responseCode = "403", description = "Forbidden", content = {@Content(schema = @Schema())})})
     public ResponseEntity<CommonRs<PersonRs>> updateUserInfo(@RequestHeader("authorization")
                                                                  @Parameter(description =  "Access Token", example = "JWT Token")
                                                                  String authorization,
-                                            @RequestBody UserRq userData) {
+                                            @RequestBody @Parameter(description =  "userData", example = "userData")
+                                            UserRq userData) {
         return personService.updateUserInfo(authorization, userData);
     }
 
     @OnlineStatusUpdatable
     @DeleteMapping("/me")
-    @Operation(summary = "delete information about me")
+    @Operation(summary = "delete information about me", responses = {
+            @ApiResponse(responseCode = "200", description = "OK",
+                    content = {@Content(schema = @Schema(ref = "#/components/schemas/CommonRsComplexRs"))}),
+            @ApiResponse(responseCode = "400", description = "Name of error",
+                    content = {@Content(schema = @Schema(implementation = ErrorRs.class))}),
+            @ApiResponse(responseCode = "401", description = "Unauthorized", content = {@Content(schema = @Schema())}),
+            @ApiResponse(responseCode = "403", description = "Forbidden", content = {@Content(schema = @Schema())})})
     public CommonRs<ComplexRs> deleteUser(@RequestHeader("authorization")
                                               @Parameter(description =  "Access Token", example = "JWT Token")
                                               String authorization) {
@@ -114,7 +147,13 @@ public class UsersController {
 
     @OnlineStatusUpdatable
     @PostMapping("/me/recover")
-    @Operation(summary = "recover information about me")
+    @Operation(summary = "recover information about me", responses = {
+            @ApiResponse(responseCode = "200", description = "OK",
+                    content = {@Content(schema = @Schema(ref = "#/components/schemas/CommonRsComplexRs"))}),
+            @ApiResponse(responseCode = "400", description = "Name of error",
+                    content = {@Content(schema = @Schema(implementation = ErrorRs.class))}),
+            @ApiResponse(responseCode = "401", description = "Unauthorized", content = {@Content(schema = @Schema())}),
+            @ApiResponse(responseCode = "403", description = "Forbidden", content = {@Content(schema = @Schema())})})
     public CommonRs<ComplexRs> recoverUser(@RequestHeader("authorization")
                                                @Parameter(description =  "Access Token", example = "JWT Token")
                                                String authorization) {
