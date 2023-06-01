35bf0c7f7f0f093114b1752bf15f444b056e4a1d
alexsandr_isakov <agisakov@mail.ru>
2023-06-01 18:54:25 +0700

    swagger 01.06.2023

--- src/main/java/socialnet/api/response/DialogRs.java
+++ src/main/java/socialnet/api/response/DialogRs.java
@@ -2,6 +2,7 @@ package socialnet.api.response;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,22 +13,28 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@Schema(description = "data about dialog")
 public class DialogRs {
 
     @JsonProperty("author_id")
+    @Schema(description = "message author id", example = "1")
     private Long authorId;
 
+    @Schema(description = "id of dialog", example = "1")
     private Long id;
 
     @JsonProperty("last_message")
     private MessageRs lastMessage;
 
     @JsonProperty("read_status")
+    @Schema(description = "read or unread message", example = "read")
     private String readStatus;
 
     @JsonProperty("recipient_id")
+    @Schema(description = "message recipient id", example = "2")
     private Long recipientId;
 
     @JsonProperty("unread_count")
+    @Schema(description = "count of unread message", example = "15")
     private Long unreadCount;
 }
